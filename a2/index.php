985d24bb2af13979d55b8f0144937b2dbd7f54fd
Andrew-Joyce <s3876520@student.rmit.edu.au>
2023-07-04 22:16:11 +1000

    fix png

--- a2/index.php
+++ a2/index.php
@@ -110,7 +110,7 @@
         <div class="movie-card" tabindex="0">
             <div class="movie-front">
                 <a href="https://www.youtube.com/watch?v=eQfMbSe7F2g" target="_blank">
-                    <img src="../media/indiana-jones-poster.pgn" alt="Indiana Jones and the Dial of Destiny">
+                    <img src="../media/indiana-jones-poster.png" alt="Indiana Jones and the Dial of Destiny">
                 </a>
                 <div>
                     <h3>Indiana Jones and the Dial of Destiny</h3>
@@ -132,7 +132,7 @@
         <div class="movie-card" tabindex="0">
             <div class="movie-front">
                 <a href="https://www.youtube.com/watch?v=pBk4NYhWNMM" target="_blank">
-                    <img src="../media/barbie-poster.pgn" alt="Barbie">
+                    <img src="../media/barbie-poster.png" alt="Barbie">
                 </a>
                 <div>
                     <h3>Barbie</h3>
@@ -153,7 +153,7 @@
         <div class="movie-card" tabindex="0">
             <div class="movie-front">
                 <a href="https://www.youtube.com/watch?v=IHvzw4Ibuho" target="_blank">
-                    <img src="../media/ninja-turtles-poster.pgn" alt="Teenage Mutant Ninja Turtles: Mutant Mayhem">
+                    <img src="../media/ninja-turtles-poster.png" alt="Teenage Mutant Ninja Turtles: Mutant Mayhem">
                 </a>
                 <div>
                     <h3>Teenage Mutant Ninja Turtles: Mutant Mayhem</h3>
@@ -175,7 +175,7 @@
         <div class="movie-card" tabindex="0">
             <div class="movie-front">
                 <a href="https://www.youtube.com/watch?v=uYPbbksJxIg" target="_blank">
-                    <img src="../media/oppenheimer-poster.pgn" alt="Oppenheimer">
+                    <img src="../media/oppenheimer-poster.png" alt="Oppenheimer">
                 </a>
                 <div>
                     <h3>Oppenheimer</h3>
