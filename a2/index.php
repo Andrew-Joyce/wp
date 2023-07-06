1579faac4f9a982cf34ac9e85049d60fbd81ed53
Andrew-Joyce <s3876520@student.rmit.edu.au>
2023-07-07 01:29:29 +1000

    remove youtube

--- a2/index.php
+++ a2/index.php
@@ -113,7 +113,6 @@
                     <li>Wed - Fri: 9pm</li>
                     <li>Sat - Sun: 6pm</li>
                 </ul>
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/eQfMbSe7F2g" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 <a href="booking.php?movie=ACT" class="book-now">Book Now</a>
             </div>
         </div>
