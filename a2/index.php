4b02142d92e95000e7ad8288435f0101eb144cf8
Andrew-Joyce <s3876520@student.rmit.edu.au>
2023-07-09 21:49:52 +1000

    Move Image of Cinema

--- a2/index.php
+++ a2/index.php
@@ -29,6 +29,9 @@
       <p>Welcome to our cinema! Nestled in the heart of our beautiful town in Australia, our cinema has been a cornerstone of the community for many years. We are ecstatic to reopen our doors after extensive improvements and renovations that have transformed our cinema into a state-of-the-art movie destination.</p>
       <h3>A Fresh New Look</h3>
       <p>Our renovations encompass a complete overhaul of the interiors with a contemporary aesthetic that still pays homage to the rich history of our cinema. From the moment you step in, you’ll be welcomed by a stylish and comfortable ambiance that sets the stage for an unforgettable movie-going experience.</p>
+        <div class="about-image">
+            <img src="../../media/Capri.png" alt="Cinema Image">
+        </div>
       <h3>World-Class Projection and Sound Systems</h3>
         <p>Our commitment to delivering the ultimate movie experience is evident in our technological upgrades. The installation of 3D Dolby Vision projection ensures every frame is razor-sharp with colors that are more vibrant. Accompanying the stunning visuals is our Dolby Atmos sound system. Whether it’s the subtlest of dialogues or the most explosive action scenes, Dolby Atmos encapsulates you in a multi-dimensional sound experience that is unparalleled.</p>
 
@@ -47,10 +50,6 @@
       <h3>Sustainable and Eco-Friendly</h3>
       <p>In line with our commitment to the environment, our renovations also include eco-friendly initiatives. Our energy-efficient lighting and optimized HVAC systems not only enhance your experience but also minimize our carbon footprint. Our commitment to sustainability also extends to using locally sourced concessions, and we are actively engaging in recycling programs.</p>
       <p>We eagerly welcome both our loyal patrons and new guests to join us in experiencing the new chapter of our cinema. With our top-of-the-line amenities and community-driven approach, we promise an unmatched movie experience that will keep you coming back for more.</p>
-    </div>
-    <div class="about-image">
-      <img src="../../media/Capri.png" alt="Cinema Image">
-    </div>
   </div>
 </section>
 
