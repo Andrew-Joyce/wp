91daeea95ead203f54fdc923ad061dddb24b4e5c
Andrew-Joyce <s3876520@student.rmit.edu.au>
2023-07-10 05:36:15 +1000

    Update gold class ticket prices

--- a2/booking.php
+++ a2/booking.php
@@ -207,6 +207,27 @@
             </div>
             </div>
         </div>
+        
+        <legend>Gold Class Seats:</legend>
+        <div id="seats-&-prices">
+            <div class="seats-container">
+            <div class="seat standard-seat">
+                <label for="seats[STA]">Standard Adult</label>
+                <input type="number" name="seats[STA]" min="0" placeholder="Enter quantity" required>
+                <span class="seat-price">Full Price: $31.00/ Discount: $25.00</span>
+            </div>
+            <div class="seat concession-seat">
+                <label for="seats[STP]">Concession</label>
+                <input type="number" name="seats[STP]" min="0" placeholder="Enter quantity" required>
+                <span class="seat-price">Full Price: $28.00 / Discount: $23.50</span>
+            </div>
+            <div class="seat child-seat">
+                <label for="seats[STC]">Child</label>
+                <input type="number" name="seats[STC]" min="0" placeholder="Enter quantity" required>
+                <span class="seat-price">Full Price: $25.00 / Discount: $22.00</span>
+            </div>
+            </div>
+        </div>
         </fieldset>
 
       <fieldset>
@@ -228,18 +249,18 @@
   </main>
 
   <footer>
-    <div class="contact-info">
-      <h3>Contact Us</h3>
-      <p><strong>Email:</strong> <a href="mailto:info@ourcinema.com">info@ourcinema.com</a></p>
-      <p><strong>Phone:</strong> <a href="tel:+61-123-456-789">+61 123 456 789</a></p>
-      <p><strong>Address:</strong> 123 Cinema Street, MovieTown, Australia</p>
+       <div class="contact-info">
+          <h3>Contact Us</h3>
+          <p><strong>Email:</strong> <a href="mailto:info@ourcinema.com">info@ourcinema.com</a></p>
+          <p><strong>Phone:</strong> <a href="tel:+61-123-456-789">+61 123 456 789</a></p>
+          <p><strong>Address:</strong> 123 Cinema Street, MovieTown, Australia</p>
     </div>
-    <div class="copyright">
-      <p>&copy; 2023 Lunardo Cinema. All rights reserved.</p>
-    </div>
-  </footer>
-</body>
-
+       <div>&copy;<script>
+          document.write(new Date().getFullYear());
+         </script>Andrew Joyce, student number - S3876520. Last modified <?= date ("Y F d  H:i", filemtime($_SERVER['SCRIPT_FILENAME'])); ?>.</div>
+         <div>Disclaimer: This website is not a real website and is being developed as part of a School of Science Web Programming course at RMIT University in Melbourne, Australia.</div>
+         <div><button id='toggleWireframeCSS' onclick='toggleWireframe()'>Toggle Wireframe CSS</button></div>
+   </footer>
+  </body>
 </html>
-
        
