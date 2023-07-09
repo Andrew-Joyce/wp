f71d3a9a65fd7b095781dd416ceed60287a16ac6
Andrew-Joyce <s3876520@student.rmit.edu.au>
2023-07-09 18:02:50 +1000

    Update Standard Seats

--- a2/index.php
+++ a2/index.php
@@ -45,59 +45,48 @@
       </div>
     </div>
   </section>
-        <section id="seats-and-prices">
-            <h2>Seats and Prices</h2>
-            <div class="table-container">
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Seat Type</th>
-                            <th>Seat Code</th>
-                            <th>Discounted Prices</th>
-                            <th>Normal Prices</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>Standard Adult</td>
-                            <td>STA</td>
-                            <td>16.00</td>
-                            <td>21.50</td>
-                        </tr>
-                        <tr>
-                            <td>Standard Concession</td>
-                            <td>STP</td>
-                            <td>14.50</td>
-                            <td>19.00</td>
-                        </tr>
-                        <tr>
-                            <td>Standard Child</td>
-                            <td>STC</td>
-                            <td>13.00</td>
-                            <td>17.50</td>
-                        </tr>
-                        <tr>
-                            <td>First Class Adult</td>
-                            <td>FCA</td>
-                            <td>25.00</td>
-                            <td>31.00</td>
-                        </tr>
-                        <tr>
-                            <td>First Class Concession</td>
-                            <td>FCP</td>
-                            <td>23.50</td>
-                            <td>28.00</td>
-                        </tr>
-                        <tr>
-                            <td>First Class Child</td>
-                            <td>FCC</td>
-                            <td>22.00</td>
-                            <td>25.00</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        </section>
+  <section id="standard-seats">
+  <div class="seat-content">
+    <div class="seat-text">
+      <h2>Standard Seats</h2>
+      <p>Our cinema offers comfortable and spacious standard seats, designed to provide you with the perfect balance of relaxation and enjoyment during your movie experience. With generous legroom and ergonomic designs, our standard seats ensure that you can sit back, unwind, and immerse yourself in the magic of cinema. Whether you're watching the latest blockbuster or a timeless classic, our standard seats guarantee a comfortable and enjoyable viewing experience for all movie enthusiasts. Sit back, relax, and let our standard seats enhance your cinematic journey.</p>
+      <table class="seats-prices-table">
+        <thead>
+          <tr>
+            <th>Seat Type</th>
+            <th>Seat Code</th>
+            <th>Discounted Prices</th>
+            <th>Normal Prices</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Standard Adult</td>
+            <td>STA</td>
+            <td>$16.00</td>
+            <td>$21.50</td>
+          </tr>
+          <tr>
+            <td>Standard Concession</td>
+            <td>STP</td>
+            <td>$14.50</td>
+            <td>$19.00</td>
+          </tr>
+          <tr>
+            <td>Standard Child</td>
+            <td>STC</td>
+            <td>$13.00</td>
+            <td>$17.50</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <div class="seat-image">
+      <img src="../../media/Profern-Standard-TWin.png" alt="Standard Seats Image">
+    </div>
+  </div>
+</section>
+
         
         <section id="now-showing">
     <h2>Now Showing</h2>
