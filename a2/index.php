e350944c8e4e790fae566a5750141cca3984be18
Andrew-Joyce <s3876520@student.rmit.edu.au>
2023-07-09 12:18:50 +1000

    Correct image name

--- a2/index.php
+++ a2/index.php
@@ -11,7 +11,7 @@
   <body>
   <header style="text-align: center;">
     <div style="display: inline-block;">
-        <img src="../../media/cinema.png" alt="Cinema">
+        <img src="../../media/Cinema.png" alt="Cinema">
         <h1 style="display: inline;">Lunardo Cinema</h1>
     </div>
     </header>
