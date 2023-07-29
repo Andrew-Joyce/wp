b22713d849d4c49888071bab53ffba0e77d92537
Andrew-Joyce <s3876520@student.rmit.edu.au>
2023-07-29 15:53:36 +1000

    Test new CSS

--- a3/booking.php
+++ a3/booking.php
@@ -44,43 +44,43 @@
     
     <form method="POST" action="" id="booking-form">
     <fieldset id="fieldset-session-ACT">
-        <div class="movie-details" id="indiana-jones-details" style="display: block;">
-            <div class="synopsis">
-            <h2>Indiana Jones and the Dial of Destiny</h2>
-            <p>Embark on an epic adventure with Indiana Jones as he searches for the mystical Dial of Destiny. Join him in a thrilling quest filled with ancient artifacts, hidden treasures, and dangerous foes. Prepare for heart-stopping action and breathtaking discoveries in this action-packed blockbuster!</p>
-            <p><strong>Starring</strong> - Harrison Ford · Phoebe Waller-Bridge · Antonio Banderas</p>
-            <p>For more information, visit <a href="https://www.imdb.com/title/tt1462764/" target="_blank">IMDb</a>.</p>
-            </div>
-            <div class="trailer">
-            <div class="video-container">
-            <iframe src="https://www.youtube.com/embed/eQfMbSe7F2g" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            </div>
-        </div> 
-        <legend>Select Session</legend>
-        <div class="session-selection">
-            <div class="session">
-            <input type="radio" name="session" id="session-1" value="9pm">
-            <label for="session-1">9pm</label>
-            <select name="day" id="day1">
-                <option value="mon">Monday</option>
-                <option value="tue">Tuesday</option>
-                <option value="wed">Wednesday</option>
-                <option value="thu">Thursday</option>
-                <option value="fri">Friday</option>
-            </select>
-            </div>
-            <div class="session">
-            <input type="radio" name="session" id="session-2" value="6pm">
-            <label for="session-2">6pm</label>
-            <select name="day" id="day2">
-                <option value="sat">Saturday</option>
-                <option value="sun">Sunday</option>
-            </select>
-            </div>
-        </div>
-        </fieldset>
+  <div class="movie-details" id="indiana-jones-details">
+    <div class="synopsis">
+      <h2>Indiana Jones and the Dial of Destiny</h2>
+      <p>Embark on an epic adventure with Indiana Jones as he searches for the mystical Dial of Destiny. Join him in a thrilling quest filled with ancient artifacts, hidden treasures, and dangerous foes. Prepare for heart-stopping action and breathtaking discoveries in this action-packed blockbuster!</p>
+      <p><strong>Starring</strong> - Harrison Ford · Phoebe Waller-Bridge · Antonio Banderas</p>
+      <p>For more information, visit <a href="https://www.imdb.com/title/tt1462764/" target="_blank">IMDb</a>.</p>
+    </div>
+    <div class="trailer">
+      <div class="video-container">
+        <iframe src="https://www.youtube.com/embed/eQfMbSe7F2g" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
+    </div>
+  </div>
 
-
+  <legend>Select Session</legend>
+  <div class="session-selection">
+    <div class="session">
+      <input type="radio" name="session" id="session-1" value="9pm">
+      <label for="session-1">9pm</label>
+      <select name="day" id="day1">
+        <option value="mon">Monday</option>
+        <option value="tue">Tuesday</option>
+        <option value="wed">Wednesday</option>
+        <option value="thu">Thursday</option>
+        <option value="fri">Friday</option>
+      </select>
+    </div>
+    <div class="session">
+      <input type="radio" name="session" id="session-2" value="6pm">
+      <label for="session-2">6pm</label>
+      <select name="day" id="day2">
+        <option value="sat">Saturday</option>
+        <option value="sun">Sunday</option>
+      </select>
+    </div>
+  </div>
+</fieldset>
         <fieldset id="fieldset-session-RMC">
         <div class="movie-details" id="barbie-details" style="display: block;">
         <div class="synopsis">
