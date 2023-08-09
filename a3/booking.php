87d70c239b13f41d9e561d0ca86eed602ff2bfaf
Andrew-Joyce <s3876520@student.rmit.edu.au>
2023-08-09 22:02:13 +1000

    fix html booking 2

--- a3/booking.php
+++ a3/booking.php
@@ -12,15 +12,12 @@ $selectedMovieScreenings = $selectedMovieDetails['screenings'];
 <!DOCTYPE html>
 <html lang="en">
 
-<?php foreach ($selectedMovieScreenings as $day => $screening) { ?>
-
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <title>Booking Form</title>
-
 </head>
 
 <body>
