e73a38a619e0fad63844b6303ab3de9693054bb5
s3876520 <S3876520@student.rmit.edu.au>
2023-08-19 16:44:50 +1000

    Add debug to index page

--- a3/booking.php
+++ a3/booking.php
@@ -165,6 +165,23 @@ if ($selectedMovieDetails) {
     </footer>
 </body>
 
+<div id="debug-module">
+    <h2>Debug Module</h2>
+    <div>
+        <h3>Request Data</h3>
+        <pre><?php print_r($_GET); ?></pre>
+        <pre><?php print_r($_POST); ?></pre>
+    </div>
+    <div>
+        <h3>Session Data</h3>
+        <pre><?php print_r($_SESSION); ?></pre>
+    </div>
+    <div>
+        <h3>Page Code</h3>
+        <pre><?php echo htmlentities(file_get_contents(__FILE__)); ?></pre>
+    </div>
+</div>
+
 </html>
 
 
--- a3/post-validation.php
+++ a3/post-validation.php
@@ -1,28 +1,72 @@
 <?php
+session_start();
+include 'tools.php';
 
-/* Call this function in the booking page like so:
-   $postErrors = validateBooking();
-   If the array is empty, then no errors were generated
-*/
-function validateBooking() {
-  $errors = []; // new empty array to return multiple error messages
-  $username = trim($_POST['user']['name']);
-  if ( $username == '') {
-    $errors['user']['name'] = "Name can't be blank";
-  } else {
-    // more advanced name checks here with better error message
-  }
-  $email = trim($_POST['user']['email']);
-  if ($email == '') {
-    $errors['user']['email'] = "Email can't be blank";
-  } else {
-    // more advanced email checks here with better error message
-  }
-  // ... repeat for all other form field checks
-
-  return $errors; // empty array -> no errors; populated array -> errors.
+function isValidEmail($email) {
+    return filter_var($email, FILTER_VALIDATE_EMAIL);
 }
 
+function isValidMobile($mobile) {
+    return preg_match("/^(?:04\d{2}\s?\d{3}\s?\d{3}|04\d{2}\s?\d{6})$/", $mobile);
+}
 
+function isValidIntegerInRange($value, $min, $max) {
+    return filter_var($value, FILTER_VALIDATE_INT, array(
+        'options' => array('min_range' => $min, 'max_range' => $max)
+    ));
+}
 
+$errors = array();
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $name = trim($_POST['name']);
+    $mobile = trim($_POST['mobile']);
+    $email = trim($_POST['email']);
+
+    $standardSeats = $_POST['seats']['STA'];
+    $concessionSeats = $_POST['seats']['STP'];
+    $childSeats = $_POST['seats']['STC'];
+
+    $firstClassAdultSeats = $_POST['seats']['FCA'];
+    $firstClassConcessionSeats = $_POST['seats']['FCP'];
+    $firstClassChildSeats = $_POST['seats']['FCC'];
+
+    if (empty($name)) {
+        $errors['name'] = "Name can't be blank";
+    }
+    if (!isValidMobile($mobile)) {
+        $errors['mobile'] = "Invalid mobile number format";
+    }
+    if (!isValidEmail($email)) {
+        $errors['email'] = "Invalid email format";
+    }
+
+    if (!isValidIntegerInRange($standardSeats, 0, 10)) {
+        $errors['standard_seats'] = "Invalid quantity for standard seats";
+    }
+    if (!isValidIntegerInRange($concessionSeats, 0, 10)) {
+        $errors['concession_seats'] = "Invalid quantity for concession seats";
+    }
+    if (!isValidIntegerInRange($childSeats, 0, 10)) {
+        $errors['child_seats'] = "Invalid quantity for child seats";
+    }
+
+    if (!isValidIntegerInRange($firstClassAdultSeats, 0, 10)) {
+        $errors['first_class_adult_seats'] = "Invalid quantity for first class adult seats";
+    }
+    if (!isValidIntegerInRange($firstClassConcessionSeats, 0, 10)) {
+        $errors['first_class_concession_seats'] = "Invalid quantity for first class concession seats";
+    }
+    if (!isValidIntegerInRange($firstClassChildSeats, 0, 10)) {
+        $errors['first_class_child_seats'] = "Invalid quantity for first class child seats";
+    }
+
+    if (empty($errors)) {
+        header("Location: receipt.php");
+        exit();
+    }
+}
+
+$_SESSION['errors'] = $errors;
+header("Location: booking.php");
+exit();
 ?>
