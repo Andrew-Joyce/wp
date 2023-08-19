6c4453360c007c400d46efa30dae8826283f766b
s3876520 <S3876520@student.rmit.edu.au>
2023-08-19 14:02:16 +1000

    Retest remember me

--- a3/script.js
+++ a3/script.js
@@ -66,91 +66,67 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 
-function rememberMe(event) {
-    console.log("Remember Me clicked");
-    event.preventDefault();
-    
-    const name = document.getElementById('name').value;
-    const mobile = document.getElementById('mobile').value;
-    const email = document.getElementById('email').value;
-  
-    localStorage.setItem('name', name);
-    localStorage.setItem('mobile', mobile);
-    localStorage.setItem('email', email);
-  
-    document.getElementById('remember-btn').classList.add('active');
-    document.getElementById('remember-btn').classList.remove('inactive');
-    document.getElementById('forget-btn').classList.remove('active');
-    document.getElementById('forget-btn').classList.add('inactive');
+function storeCustomerDetails(name, email, mobile) {
+    const customerDetails = {
+        name: name,
+        email: email,
+        mobile: mobile,
+    };
+    localStorage.setItem('customerDetails', JSON.stringify(customerDetails));
 }
 
-function forgetMe(event) {
-    console.log("Forget Me clicked");
-    event.preventDefault();
-  
-    localStorage.removeItem('name');
-    localStorage.removeItem('mobile');
-    localStorage.removeItem('email');
-  
-    document.getElementById('remember-btn').classList.remove('active');
-    document.getElementById('remember-btn').classList.add('inactive');
-    document.getElementById('forget-btn').classList.add('active');
-    document.getElementById('forget-btn').classList.remove('inactive');
+function getStoredCustomerDetails() {
+    const storedCustomerDetails = localStorage.getItem('customerDetails');
+    if (storedCustomerDetails) {
+        return JSON.parse(storedCustomerDetails);
+    }
+    return null;
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-    if (localStorage.getItem('name')) {
-        document.getElementById('name').value = localStorage.getItem('name');
-        document.getElementById('mobile').value = localStorage.getItem('mobile');
-        document.getElementById('email').value = localStorage.getItem('email');
-  
+function removeStoredCustomerDetails() {
+    localStorage.removeItem('customerDetails');
+}
+
+function updateFormWithStoredDetails() {
+    const storedDetails = getStoredCustomerDetails();
+    if (storedDetails) {
+        document.getElementById('name').value = storedDetails.name;
+        document.getElementById('email').value = storedDetails.email;
+        document.getElementById('mobile').value = storedDetails.mobile;
         document.getElementById('remember-btn').classList.add('active');
-        document.getElementById('remember-btn').classList.remove('inactive');
         document.getElementById('forget-btn').classList.remove('active');
-        document.getElementById('forget-btn').classList.add('inactive');
-    }
-
-    if (window.location.pathname.endsWith('booking.php')) {
-        document.getElementById('remember-btn').addEventListener('click', function(event) {
-            rememberMe(event); 
-        });
-        document.getElementById('forget-btn').addEventListener('click', function(event) {
-            forgetMe(event);
-        });
-    }
-});
-
-
-document.addEventListener('DOMContentLoaded', function () {
-    const sections = document.querySelectorAll('article'); 
-    const navLinks = document.querySelectorAll('.nav-section');
-    
-    window.addEventListener('scroll', function () {
-      sections.forEach((section, index) => {
-        const rect = section.getBoundingClientRect();
-        const threshold = rect.height * 0.5;
-        
-        if (rect.top <= threshold && rect.bottom >= threshold) {
-          navLinks.forEach(navLink => {
-            navLink.classList.remove('active');
-            navLink.style.color = 'white'; 
-          });
-          
-          navLinks[index].classList.add('active');
-          navLinks[index].style.color = 'blue';
-        }
-      });
-    });
-  });
-  
-  function toggleButton(button) {
-    button.classList.toggle('active');
-    button.classList.toggle('inactive');
-  
-    if (button.classList.contains('active')) {
-      button.style.color = 'blue';
     } else {
-      button.style.color = 'initial'; 
+        document.getElementById('remember-btn').classList.remove('active');
+        document.getElementById('forget-btn').classList.add('active');
     }
-    return false; 
-  }
+}
+
+function toggleButton(button) {
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const mobile = document.getElementById('mobile').value;
+
+    if (button.id === 'remember-btn') {
+        storeCustomerDetails(name, email, mobile);
+    } else {
+        removeStoredCustomerDetails();
+    }
+
+    updateFormWithStoredDetails();
+}
+
+function initializeForm() {
+    updateFormWithStoredDetails();
+}
+
+window.onload = function() {
+    document.getElementById('remember-btn').addEventListener('click', function() {
+        toggleButton(this);
+    });
+    document.getElementById('forget-btn').addEventListener('click', function() {
+        toggleButton(this);
+    });
+
+    initializeForm();
+};
+
