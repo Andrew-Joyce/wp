22b8fb716daa77e35e89e64bc3df115e3f45f883
s3876520 <S3876520@student.rmit.edu.au>
2023-08-20 12:56:08 +1000

    FIX JS Logs

--- a3/script.js
+++ a3/script.js
@@ -169,29 +169,3 @@ document.getElementById('booking-form').addEventListener('submit', function(even
         console.error('Form submission error:', error);
     });
 });
-
-
-function validateForm() {
-
-}
-
-document.getElementById('booking-form').addEventListener('submit', function(event) {
-  event.preventDefault();
-  
-  if (validateForm()) {
-      fetch('submit.php', {
-          method: 'POST',
-          body: new FormData(document.getElementById('booking-form'))
-      })
-      .then(response => response.text())
-      .then(data => {
-          console.log('Form submission response:', data);
-      })
-      .catch(error => {
-          console.error('Form submission error:', error);
-      });
-  } else {
-      console.log('Form is not valid');
-  }
-});
-
