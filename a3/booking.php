1da64428cc86b07ab4d6129d4398d311d0de0423
s3876520 <S3876520@student.rmit.edu.au>
2023-08-21 19:14:51 +1000

    update button type

--- a3/booking.php
+++ a3/booking.php
@@ -138,8 +138,8 @@ if ($selectedMovieDetails) {
             <input type="email" name="email" id="email" required>
         </div>
         <div>
-            <button id="remember-btn" class="contact-Button active" onclick="rememberMe()">Remember Me</button>
-            <button id="forget-btn" class="contact-Button inactive" onclick="forgetMe()">Forget Me</button>
+            <button type="button" id="remember-btn" class="contact-Button active" onclick="rememberMe()">Remember Me</button>
+            <button type="button" id="forget-btn" class="contact-Button inactive" onclick="forgetMe()">Forget Me</button>
         </div>
     </fieldset>
 
