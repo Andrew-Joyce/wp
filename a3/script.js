cd274fe3866f209aa170de63171bf50e4dfe0321
s3876520 <S3876520@student.rmit.edu.au>
2023-08-21 22:51:52 +1000

    test change to JS

--- a3/script.js
+++ a3/script.js
@@ -1,31 +1,22 @@
-const urlParams = new URLSearchParams(window.location.search);
-const selectedMovie = urlParams.get('movie');
+$(document).ready(function() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const selectedMovie = urlParams.get('movie');
 
-document.addEventListener('DOMContentLoaded', () => {
-    const sessionFieldsets = document.querySelectorAll('fieldset[id^="fieldset-session"]');
-    sessionFieldsets.forEach((fieldset) => {
-        fieldset.style.display = 'none';
-    });
+    $('fieldset[id^="fieldset-session"]').hide();
 
     if (selectedMovie) {
-        const selectedFieldset = document.getElementById(`fieldset-session-${selectedMovie}`);
-        if (selectedFieldset) {
-            selectedFieldset.style.display = 'block';
+        const selectedFieldset = $(`#fieldset-session-${selectedMovie}`);
+        if (selectedFieldset.length) {
+            selectedFieldset.show();
         }
     }
-});
 
-document.addEventListener('DOMContentLoaded', function () {
-    var sessions = document.querySelectorAll('.session');
+    $(".session").click(function () {
+        var sessionData = $(this).data("session");
+        $("#selected-session").val(sessionData);
 
-    sessions.forEach(function(session) {
-        session.addEventListener('click', function(e) {
-            sessions.forEach(function(innerSession) {
-                innerSession.classList.remove('selected');
-            });
-
-            e.currentTarget.classList.add('selected');
-        });
+        $(".session").removeClass('selected');
+        $(this).addClass('selected');
     });
 });
 
