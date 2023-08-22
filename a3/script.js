b791e46aa475bfc95371fff9eeed87f0dfeecd0b
s3876520 <S3876520@student.rmit.edu.au>
2023-08-22 11:17:01 +1000

    reverse changes

--- a3/script.js
+++ a3/script.js
@@ -1,15 +1,8 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const urlParams = new URLSearchParams(window.location.search);
-    const selectedMovie = urlParams.get('movie');
+const urlParams = new URLSearchParams(window.location.search);
+const selectedMovie = urlParams.get('movie');
 
+document.addEventListener('DOMContentLoaded', () => {
     const sessionFieldsets = document.querySelectorAll('fieldset[id^="fieldset-session"]');
-    const sessionButtons = document.querySelectorAll('.session');
-    const ticketInputs = document.querySelectorAll('input[type="number"]');
-    const nameInput = document.getElementById('name');
-    const mobileInput = document.getElementById('mobile');
-    const emailInput = document.getElementById('email');
-    const bookingForm = document.getElementById("booking-form");
-
     sessionFieldsets.forEach((fieldset) => {
         fieldset.style.display = 'none';
     });
@@ -20,16 +13,25 @@ document.addEventListener("DOMContentLoaded", function () {
             selectedFieldset.style.display = 'block';
         }
     }
+});
 
-    sessionButtons.forEach(function (session) {
-        session.addEventListener('click', function (e) {
-            sessionButtons.forEach(function (innerSession) {
+document.addEventListener('DOMContentLoaded', function () {
+    var sessions = document.querySelectorAll('.session');
+
+    sessions.forEach(function(session) {
+        session.addEventListener('click', function(e) {
+            sessions.forEach(function(innerSession) {
                 innerSession.classList.remove('selected');
             });
 
             e.currentTarget.classList.add('selected');
         });
     });
+});
+
+    
+document.addEventListener("DOMContentLoaded", function() {
+    const ticketInputs = document.querySelectorAll('input[type="number"]');
 
     function updateTotalPrice() {
         let totalPrice = 0;
@@ -65,7 +67,21 @@ document.addEventListener("DOMContentLoaded", function () {
         input.addEventListener('input', updateTotalPrice);
     });
 
-    bookingForm.addEventListener('submit', event => {
+    const sessionButtons = document.querySelectorAll('.session');
+    sessionButtons.forEach(button => {
+        button.addEventListener('click', function() {
+            sessionButtons.forEach(btn => btn.classList.remove('error'));
+            
+            sessionButtons.forEach(btn => btn.classList.remove('selected'));
+            button.classList.add('selected');
+            
+            const selectedSessionValue = button.getAttribute('data-session');
+            document.getElementById('selected-session-input').value = selectedSessionValue;
+        });
+    });
+
+    const form = document.querySelector('form');
+    form.addEventListener('submit', event => {
         const totalSeatsSelected = updateTotalPrice();
         if (totalSeatsSelected > 10) {
             event.preventDefault();
@@ -73,41 +89,47 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 
-    function rememberMe(event) {
-        event.preventDefault();
-        
-        const name = nameInput.value;
-        const mobile = mobileInput.value;
-        const email = emailInput.value;
+    updateTotalPrice();
+});
 
-        localStorage.setItem('name', name);
-        localStorage.setItem('mobile', mobile);
-        localStorage.setItem('email', email);
+function rememberMe(event) {
+    console.log("Remember Me clicked");
+    event.preventDefault();
+    
+    const name = document.getElementById('name').value;
+    const mobile = document.getElementById('mobile').value;
+    const email = document.getElementById('email').value;
+  
+    localStorage.setItem('name', name);
+    localStorage.setItem('mobile', mobile);
+    localStorage.setItem('email', email);
+  
+    document.getElementById('remember-btn').classList.add('active');
+    document.getElementById('remember-btn').classList.remove('inactive');
+    document.getElementById('forget-btn').classList.remove('active');
+    document.getElementById('forget-btn').classList.add('inactive');
+}
 
-        document.getElementById('remember-btn').classList.add('active');
-        document.getElementById('remember-btn').classList.remove('inactive');
-        document.getElementById('forget-btn').classList.remove('active');
-        document.getElementById('forget-btn').classList.add('inactive');
-    }
+function forgetMe(event) {
+    console.log("Forget Me clicked");
+    event.preventDefault();
+  
+    localStorage.removeItem('name');
+    localStorage.removeItem('mobile');
+    localStorage.removeItem('email');
+  
+    document.getElementById('remember-btn').classList.remove('active');
+    document.getElementById('remember-btn').classList.add('inactive');
+    document.getElementById('forget-btn').classList.add('active');
+    document.getElementById('forget-btn').classList.remove('inactive');
+}
 
-    function forgetMe(event) {
-        event.preventDefault();
-
-        localStorage.removeItem('name');
-        localStorage.removeItem('mobile');
-        localStorage.removeItem('email');
-
-        document.getElementById('remember-btn').classList.remove('active');
-        document.getElementById('remember-btn').classList.add('inactive');
-        document.getElementById('forget-btn').classList.add('active');
-        document.getElementById('forget-btn').classList.remove('inactive');
-    }
-
+document.addEventListener("DOMContentLoaded", function() {
     if (localStorage.getItem('name')) {
-        nameInput.value = localStorage.getItem('name');
-        mobileInput.value = localStorage.getItem('mobile');
-        emailInput.value = localStorage.getItem('email');
-
+        document.getElementById('name').value = localStorage.getItem('name');
+        document.getElementById('mobile').value = localStorage.getItem('mobile');
+        document.getElementById('email').value = localStorage.getItem('email');
+  
         document.getElementById('remember-btn').classList.add('active');
         document.getElementById('remember-btn').classList.remove('inactive');
         document.getElementById('forget-btn').classList.remove('active');
@@ -115,69 +137,109 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     if (window.location.pathname.endsWith('booking.php')) {
-        document.getElementById('remember-btn').addEventListener('click', rememberMe);
-        document.getElementById('forget-btn').addEventListener('click', forgetMe);
-    }
-
-    function validateForm() {
-        console.log('Starting form validation...');
-
-        nameInput.classList.remove('error');
-        mobileInput.classList.remove('error');
-        emailInput.classList.remove('error');
-        sessionButtons.forEach(button => button.classList.remove('error'));
-        ticketInputs.forEach(input => input.classList.remove('error'));
-
-        let isValid = true;
-
-        if (nameInput.value.trim() === '') {
-            console.log('Name is empty.');
-            nameInput.classList.add('error');
-            isValid = false;
-        }
-
-        let mobilePattern = /^(?:04\d{2}\s?\d{3}\s?\d{3}|04\d{2}\s?\d{6})$/;
-        if (!mobilePattern.test(mobileInput.value)) {
-            console.log('Mobile format is invalid.');
-            mobileInput.classList.add('error');
-            isValid = false;
-        }
-
-        let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        if (!emailPattern.test(emailInput.value)) {
-            console.log('Email format is invalid.');
-            emailInput.classList.add('error');
-            isValid = false;
-        }
-
-        let selectedSession = document.querySelector('.session.selected');
-        if (!selectedSession) {
-            console.log('No session selected.');
-            sessionButtons.forEach(button => button.classList.add('error'));
-            isValid = false;
-        }
-
-        ticketInputs.forEach(input => {
-            let quantity = parseInt(input.value, 10);
-            if (quantity < 0) {
-                console.log('Invalid seat quantity: ' + quantity);
-                input.classList.add('error');
-                isValid = false;
-            }
+        document.getElementById('remember-btn').addEventListener('click', function(event) {
+            rememberMe(event); 
         });
-
-        console.log('Validation result: ' + (isValid ? 'Valid' : 'Invalid'));
-        return isValid;
+        document.getElementById('forget-btn').addEventListener('click', function(event) {
+            forgetMe(event);
+        });
     }
+});
 
+
+document.addEventListener('DOMContentLoaded', function () {
+    const sections = document.querySelectorAll('article'); 
+    const navLinks = document.querySelectorAll('.nav-section');
+    
+    window.addEventListener('scroll', function () {
+
+      sections.forEach((section, index) => {
+        const rect = section.getBoundingClientRect();
+        
+        const threshold = rect.height * 0.5;
+        
+        if (rect.top <= threshold && rect.bottom >= threshold) {
+
+          navLinks.forEach(navLink => {
+            navLink.classList.remove('active');
+            navLink.style.color = 'white'; 
+          });
+          
+          navLinks[index].classList.add('active');
+          navLinks[index].style.color = 'blue';
+        }
+      });
+    });
+  });
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const bookingForm = document.getElementById("booking-form");
     bookingForm.addEventListener("submit", function (event) {
         event.preventDefault();
-
+        
         const isValid = validateForm();
-
+        
         if (isValid) {
             bookingForm.submit();
         }
     });
+  });
+function validateForm() {
+    var nameInput = document.getElementById('name');
+    var mobileInput = document.getElementById('mobile');
+    var emailInput = document.getElementById('email');
+    var sessionButtons = document.querySelectorAll('.session');
+    var seatInputs = document.querySelectorAll('input[name^="seats["]');
+    
+    console.log('Starting form validation...');
 
-});
+    nameInput.classList.remove('error');
+    mobileInput.classList.remove('error');
+    emailInput.classList.remove('error');
+    sessionButtons.forEach(button => button.classList.remove('error'));
+    seatInputs.forEach(input => input.classList.remove('error'));
+    
+    var isValid = true;
+    
+    if (nameInput.value.trim() === '') {
+        console.log('Name is empty.');
+        nameInput.classList.add('error');
+        isValid = false;
+    }
+    
+    var mobilePattern = /^(?:04\d{2}\s?\d{3}\s?\d{3}|04\d{2}\s?\d{6})$/;
+    if (!mobilePattern.test(mobileInput.value)) {
+        console.log('Mobile format is invalid.');
+        mobileInput.classList.add('error');
+        isValid = false;
+    }
+    
+    var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailPattern.test(emailInput.value)) {
+        console.log('Email format is invalid.');
+        emailInput.classList.add('error');
+        isValid = false;
+    }
+    
+    var selectedSession = document.querySelector('.session.selected');
+    if (!selectedSession) {
+        console.log('No session selected.');
+        sessionButtons.forEach(button => button.classList.add('error'));
+        isValid = false;
+    }
+    
+    seatInputs.forEach(input => {
+        var quantity = parseInt(input.value, 10);
+        if (quantity < 0) {
+            console.log('Invalid seat quantity: ' + quantity);
+            input.classList.add('error');
+            isValid = false;
+        }
+    });
+    
+    console.log('Validation result: ' + (isValid ? 'Valid' : 'Invalid'));
+    return isValid;
+}
+
+
+
