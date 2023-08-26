0cbacb9e729dfb8acfaa5cb2515dda5f6169cbc0
s3876520 <S3876520@student.rmit.edu.au>
2023-08-26 20:21:13 +1000

    update submit

--- a4/submit.php
+++ a4/submit.php
@@ -1,57 +1,83 @@
+<?php
+session_start();
+
+if (!isset($_SESSION["booking_data"])) {
+    header("Location: index.php");
+    exit();
+}
+
+$bookingData = $_SESSION["booking_data"];
+
+unset($_SESSION["booking_data"]);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title>Booking Form</title>
-</head>
-
-<body>
-    <header style="text-align: center;">
-        <div style="display: flex; justify-content: center; align-items: center;">
-            <img src="../../media/Cinema.png" alt="Cinema" class="responsive-image" style="margin-right: 10px;">
-            <h1 style="display: inline;">Lunardo Cinema</h1>
-        </div>
-    </header>
-
-    <nav id="navbar">
-        <a href="https://titan.csit.rmit.edu.au/~s3876520/wp/a3/index.php#now-showing">Now Showing</a>
-        <a href="https://titan.csit.rmit.edu.au/~s3876520/wp/a3/index.php#seats-&-prices">Seats & Prices</a>
-        <a href="https://titan.csit.rmit.edu.au/~s3876520/wp/a3/index.php#about-us">About Us</a>
-    </nav>
-
-    <h1>Booking Summary</h1>
-
-    <?php
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $movie = $_POST["movie"];
-        $session = $_POST["session"];
-        $seats = $_POST["seats"];
-        $name = $_POST["name"];
-        $mobileNumber = $_POST["mobile"];
-        $email = $_POST["email"];
-
-        echo "PHP is working!";
-
-        echo "<p><strong>Movie:</strong> $movie</p>";
-        echo "<p><strong>Session:</strong> $session</p>";
-
-        echo "<p><strong>Selected Seats:</strong></p>";
-        echo "<ul>";
-        foreach ($seats as $seatType => $quantity) {
-            if (!empty($quantity)) {
-                echo "<li>$seatType: $quantity</li>";
-            }
+    <title>Receipt</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f5f5f5;
+            margin: 0;
+            padding: 0;
         }
-        echo "</ul>";
+        .receipt-container {
+            max-width: 800px;
+            margin: 0 auto;
+            background-color: white;
+            padding: 20px;
+            box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+        }
+    </style>
+</head>
+<body>
+    <div class="receipt-container">
+        <header>
+            <img src="../../media/Cinema.png" alt="Cinema Logo">
+            <h1>Lunardo Cinema</h1>
+        </header>
+        
+        <main>
+            <h2>Receipt</h2>
+            <h3>Customer Details</h3>
+            <p><strong>Name:</strong> <?php echo $bookingData["name"]; ?></p>
+            <p><strong>Email:</strong> <?php echo $bookingData["email"]; ?></p>
+            <p><strong>Mobile:</strong> <?php echo $bookingData["mobile"]; ?></p>
 
-        echo "<p><strong>Full Name:</strong> $name</p>";
-        echo "<p><strong>Mobile Number:</strong> $mobileNumber</p>";
-        echo "<p><strong>Email:</strong> $email</p>";
-    }
-    ?>
+            <h3>Booking Details</h3>
+            <table>
+                <tr>
+                    <th>Seat Type</th>
+                    <th>Quantity</th>
+                    <th>Subtotal</th>
+                </tr>
+                <?php foreach ($bookingData["seats"] as $seatType => $quantity): ?>
+                <tr>
+                    <td><?php echo $seatType; ?></td>
+                    <td><?php echo $quantity; ?></td>
+                    <td><?php echo $quantity * $bookingData["seat_prices"][$seatType]; ?></td>
+                </tr>
+                <?php endforeach; ?>
+                <tr>
+                    <td colspan="2">Total</td>
+                    <td><?php echo array_sum(array_map(function($quantity, $price) { return $quantity * $price; }, $bookingData["seats"], $bookingData["seat_prices"])); ?></td>
+                </tr>
+                <tr>
+                    <td colspan="2">GST (10%)</td>
+                    <td><?php echo array_sum(array_map(function($quantity, $price) { return $quantity * $price; }, $bookingData["seats"], $bookingData["seat_prices"])) / 11; ?></td>
+                </tr>
+            </table>
+        </main>
 
+        <footer>
+            <p>Company Address: 123 Cinema St, City</p>
+            <p>Email: info@lunardocinema.com</p>
+            <p>Phone: 123-456-7890</p>
+        </footer>
+    </div>
 </body>
 </html>
