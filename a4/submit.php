e980ba462f3e7bdbef481f4b33f05d289100ca7f
s3876520 <S3876520@student.rmit.edu.au>
2023-08-26 17:57:58 +1000

    update php

--- a4/submit.php
+++ a4/submit.php
@@ -1,57 +1,57 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $movie = $_POST["movie"];
-    $session = $_POST["session"];
-    $seats = $_POST["seats"];
-    $name = $_POST["name"];
-    $mobileNumber = $_POST["mobile"];
-    $email = $_POST["email"];
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Booking Form</title>
+</head>
 
-    echo "PHP is working!";
+<body>
+    <header style="text-align: center;">
+        <div style="display: flex; justify-content: center; align-items: center;">
+            <img src="../../media/Cinema.png" alt="Cinema" class="responsive-image" style="margin-right: 10px;">
+            <h1 style="display: inline;">Lunardo Cinema</h1>
+        </div>
+    </header>
 
-    echo "<!DOCTYPE html>
-    <html lang='en'>
-    <head>
-        <meta name='viewport' content='width=device-width, initial-scale=1'>
-        <meta charset='UTF-8'>
-        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-        <link rel='stylesheet' href='style.css'>
-        <title>Booking Form</title>
-    </head>
+    <nav id="navbar">
+        <a href="https://titan.csit.rmit.edu.au/~s3876520/wp/a3/index.php#now-showing">Now Showing</a>
+        <a href="https://titan.csit.rmit.edu.au/~s3876520/wp/a3/index.php#seats-&-prices">Seats & Prices</a>
+        <a href="https://titan.csit.rmit.edu.au/~s3876520/wp/a3/index.php#about-us">About Us</a>
+    </nav>
 
-    <body>
-        <header style='text-align: center;'>
-            <div style='display: flex; justify-content: center; align-items: center;'>
-                <img src='../../media/Cinema.png' alt='Cinema' class='responsive-image' style='margin-right: 10px;'>
-                <h1 style='display: inline;'>Lunardo Cinema</h1>
-            </div>
-        </header>
+    <h1>Booking Summary</h1>
 
-        <nav id='navbar'>
-            <a href='https://titan.csit.rmit.edu.au/~s3876520/wp/a3/index.php#now-showing'>Now Showing</a>
-            <a href='https://titan.csit.rmit.edu.au/~s3876520/wp/a3/index.php#seats-&-prices'>Seats & Prices</a>
-            <a href='https://titan.csit.rmit.edu.au/~s3876520/wp/a3/index.php#about-us'>About Us</a>
-        </nav>
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $movie = $_POST["movie"];
+        $session = $_POST["session"];
+        $seats = $_POST["seats"];
+        $name = $_POST["name"];
+        $mobileNumber = $_POST["mobile"];
+        $email = $_POST["email"];
 
-        <h1>Booking Summary</h1>";
+        echo "PHP is working!";
 
-    echo "<p><strong>Movie:</strong> $movie</p>";
-    echo "<p><strong>Session:</strong> $session</p>";
+        echo "<p><strong>Movie:</strong> $movie</p>";
+        echo "<p><strong>Session:</strong> $session</p>";
 
-    echo "<p><strong>Selected Seats:</strong></p>";
-    echo "<ul>";
-    foreach ($seats as $seatType => $quantity) {
-        if (!empty($quantity)) {
-            echo "<li>$seatType: $quantity</li>";
+        echo "<p><strong>Selected Seats:</strong></p>";
+        echo "<ul>";
+        foreach ($seats as $seatType => $quantity) {
+            if (!empty($quantity)) {
+                echo "<li>$seatType: $quantity</li>";
+            }
         }
+        echo "</ul>";
+
+        echo "<p><strong>Full Name:</strong> $name</p>";
+        echo "<p><strong>Mobile Number:</strong> $mobileNumber</p>";
+        echo "<p><strong>Email:</strong> $email</p>";
     }
-    echo "</ul>";
+    ?>
 
-    echo "<p><strong>Full Name:</strong> $name</p>";
-    echo "<p><strong>Mobile Number:</strong> $mobileNumber</p>";
-    echo "<p><strong>Email:</strong> $email</p>";
-
-    echo "</body>
-    </html>";
-}
-?>
+</body>
+</html>
