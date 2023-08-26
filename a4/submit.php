62a4271967994b26a448ffdc3c23ac04e7cf4349
s3876520 <S3876520@student.rmit.edu.au>
2023-08-26 17:31:13 +1000

    update submit

--- a4/submit.php
+++ a4/submit.php
@@ -1,40 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles.css">
-    <title>Booking Summary</title>
-</head>
+<header style="text-align: center;">
+        <div style="display: flex; justify-content: center; align-items: center;">
+            <img src="../../media/Cinema.png" alt="Cinema" class="responsive-image" style="margin-right: 10px;">
+            <h1 style="display: inline;">Lunardo Cinema</h1>
+        </div>
+</header>
 <body>
     <h1>Booking Summary</h1>
 
+    <pre>
     <?php
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $movie = $_POST["movie"];
-        $session = $_POST["session"];
-        $seats = $_POST["seats"];
-        $name = $_POST["name"];
-        $mobileNumber = $_POST["mobile"];
-        $email = $_POST["email"];
-
-        echo "<p><strong>Movie:</strong> $movie</p>";
-        echo "<p><strong>Session:</strong> $session</p>";
-
-        echo "<p><strong>Selected Seats:</strong></p>";
-        echo "<ul>";
-        foreach ($seats as $seatType => $quantity) {
-            if (!empty($quantity)) {
-                echo "<li>$seatType: $quantity</li>";
-            }
-        }
-        echo "</ul>";
-
-        echo "<p><strong>Full Name:</strong> $name</p>";
-        echo "<p><strong>Mobile Number:</strong> $mobileNumber</p>";
-        echo "<p><strong>Email:</strong> $email</p>";
-    }
+    print_r($_POST);
     ?>
+    </pre>
 
 </body>
 </html>
+
