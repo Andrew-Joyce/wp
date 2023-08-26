69341913b10800cae3696cfdc94295109a5d8b14
s3876520 <S3876520@student.rmit.edu.au>
2023-08-26 17:46:02 +1000

    add php to submit

--- a4/submit.php
+++ a4/submit.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <script src="script.js"></script>
     <title>Booking Form</title>
 </head>
 
@@ -22,35 +21,35 @@
         <a href="https://titan.csit.rmit.edu.au/~s3876520/wp/a3/index.php#seats-&-prices">Seats & Prices</a>
         <a href="https://titan.csit.rmit.edu.au/~s3876520/wp/a3/index.php#about-us">About Us</a>
     </nav>
-    
+
     <h1>Booking Summary</h1>
 
     <?php
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $movie = $_POST["movie"];
-        $session = $_POST["session"];
-        $seats = $_POST["seats"];
-        $name = $_POST["name"];
-        $mobileNumber = $_POST["mobile"];
-        $email = $_POST["email"];
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $movie = $_POST["movie"];
+            $session = $_POST["session"];
+            $seats = $_POST["seats"];
+            $name = $_POST["name"];
+            $mobileNumber = $_POST["mobile"];
+            $email = $_POST["email"];
 
-        echo "<p><strong>Movie:</strong> $movie</p>";
-        echo "<p><strong>Session:</strong> $session</p>";
+            echo "<p><strong>Movie:</strong> $movie</p>";
+            echo "<p><strong>Session:</strong> $session</p>";
 
-        echo "<p><strong>Selected Seats:</strong></p>";
-        echo "<ul>";
-        foreach ($seats as $seatType => $quantity) {
-            if (!empty($quantity)) {
-                echo "<li>$seatType: $quantity</li>";
+            echo "<p><strong>Selected Seats:</strong></p>";
+            echo "<ul>";
+            foreach ($seats as $seatType => $quantity) {
+                if (!empty($quantity)) {
+                    echo "<li>$seatType: $quantity</li>";
+                }
             }
-        }
-        echo "</ul>";
+            echo "</ul>";
 
-        echo "<p><strong>Full Name:</strong> $name</p>";
-        echo "<p><strong>Mobile Number:</strong> $mobileNumber</p>";
-        echo "<p><strong>Email:</strong> $email</p>";
-    }
-    ?>
+            echo "<p><strong>Full Name:</strong> $name</p>";
+            echo "<p><strong>Mobile Number:</strong> $mobileNumber</p>";
+            echo "<p><strong>Email:</strong> $email</p>";
+        }
+?>
 
 </body>
 </html>
