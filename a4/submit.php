f813332763cedc7d05b901fa2328a459b3e44876
s3876520 <S3876520@student.rmit.edu.au>
2023-08-26 17:16:57 +1000

    update submit

--- a4/submit.php
+++ a4/submit.php
@@ -1,15 +1,9 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $movie = $_POST["movie"]; 
+    $movie = $_POST["movie"];
     $session = $_POST["session"];
 
-    $standardAdultSeats = $_POST["seats"]["STA-dis"];
-    $standardConcessionSeats = $_POST["seats"]["STP-dis"];
-    $standardChildSeats = $_POST["seats"]["STC-dis"];
-
-    $goldClassAdultSeats = $_POST["seats"]["FCA-dis"];
-    $goldClassConcessionSeats = $_POST["seats"]["FCP-dis"];
-    $goldClassChildSeats = $_POST["seats"]["FCC-dis"];
+    $seats = $_POST["seats"];
 
     $fullName = $_POST["name"];
     $mobileNumber = $_POST["mobile"];
@@ -18,16 +12,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     echo "Movie: " . $movie . "<br>";
     echo "Session: " . $session . "<br>";
 
-    echo "Standard Adult Seats: " . $standardAdultSeats . "<br>";
-    echo "Standard Concession Seats: " . $standardConcessionSeats . "<br>";
-    echo "Standard Child Seats: " . $standardChildSeats . "<br>";
+    echo "Standard Adult Seats: " . $seats["STA"] . "<br>";
+    echo "Standard Concession Seats: " . $seats["STP"] . "<br>";
+    echo "Standard Child Seats: " . $seats["STC"] . "<br>";
 
-    echo "Gold Class Adult Seats: " . $goldClassAdultSeats . "<br>";
-    echo "Gold Class Concession Seats: " . $goldClassConcessionSeats . "<br>";
-    echo "Gold Class Child Seats: " . $goldClassChildSeats . "<br>";
+    echo "Gold Class Adult Seats: " . $seats["FCA"] . "<br>";
+    echo "Gold Class Concession Seats: " . $seats["FCP"] . "<br>";
+    echo "Gold Class Child Seats: " . $seats["FCC"] . "<br>";
 
     echo "Full Name: " . $fullName . "<br>";
     echo "Mobile Number: " . $mobileNumber . "<br>";
     echo "Email: " . $email . "<br>";
 }
+
 ?>
