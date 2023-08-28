1dd37fc8021c20839390636086efba52deaa2260
s3876520 <S3876520@student.rmit.edu.au>
2023-08-28 22:36:21 +1000

    update ticket to a container

--- a4/submit.php
+++ a4/submit.php
@@ -74,17 +74,20 @@ unset($_SESSION["booking_data"]);
             <h2>Your Tickets</h2>
             <?php foreach ($seatsData as $seatType => $quantity): ?>
                 <?php for ($i = 0; $i < $quantity; $i++): ?>
-                    <div class="ticket <?= ($seatType == "gold") ? "gold" : ""; ?>">
-                        <img src="<?php echo getPosterPath(getMovieDetails($bookingData["movie_code"])["title"]); ?>" alt="<?php echo getMovieDetails($bookingData["movie_code"])["title"]; ?>">
-                        <h3><?php echo getMovieDetails($bookingData["movie_code"])["title"]; ?></h3>
-                        <p><strong>Seat Type:</strong> <?php echo convertSeatType($seatType); ?></p>
-                        <p><strong>Session:</strong> <?php echo $formattedSession; ?></p>
-                        <p><strong>Seat Number:</strong> <?php echo ($i + 1); ?></p>
+                    <div class="ticket-container">
+                        <div class="ticket <?= ($seatType == "gold") ? "gold" : "standard"; ?>">
+                            <img src="<?php echo getPosterPath(getMovieDetails($bookingData["movie_code"])["title"]); ?>" alt="<?php echo getMovieDetails($bookingData["movie_code"])["title"]; ?>">
+                            <div class="ticket-details">
+                                <h3><?php echo getMovieDetails($bookingData["movie_code"])["title"]; ?></h3>
+                                <p><strong>Seat Type:</strong> <?php echo convertSeatType($seatType); ?></p>
+                                <p><strong>Session:</strong> <?php echo $formattedSession; ?></p>
+                                <p><strong>Seat Number:</strong> <?php echo ($i + 1); ?></p>
+                            </div>
+                        </div>
                     </div>
                 <?php endfor; ?>
             <?php endforeach; ?>
         </section>
-
 
         <footer>
         <div class="contact-info">
