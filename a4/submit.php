c1bdf6f895020c106d8c712d02ecdfc4dccea97d
s3876520 <S3876520@student.rmit.edu.au>
2023-08-29 20:12:20 +1000

    increase ticket size

--- a4/submit.php
+++ a4/submit.php
@@ -31,7 +31,7 @@ unset($_SESSION["booking_data"]);
 
         .ticket-grid {
             display: grid;
-            grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+            grid-template-columns: repeat(auto-fill, minmax(350px, 1fr));
             gap: 20px;
         }
 
