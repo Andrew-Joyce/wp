d3d47180f17d9d8db79ed4fd8c2226b960867b66
s3876520 <S3876520@student.rmit.edu.au>
2023-08-30 22:46:48 +1000

    revert change

--- a4/submit.php
+++ a4/submit.php
@@ -19,6 +19,7 @@ $formattedSession = formatSession($bookingData["session"]);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <style>
         .page-break {
             break-after: always;
