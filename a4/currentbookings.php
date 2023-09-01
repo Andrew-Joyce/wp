f3b6ee238cd7d7a3131983d8b04bebff547c8577
s3876520 <S3876520@student.rmit.edu.au>
2023-09-01 18:38:09 +0800

    count defect fix

--- a4/currentbookings.php
+++ a4/currentbookings.php
@@ -68,19 +68,10 @@ if (file_exists($filePath)) {
 
         foreach ($bookings as $booking) {
             $movieDetails = getMovieDetails($booking['movie']);
-            $movieTitle = isset($movieDetails['title']) ? $movieDetails['title'] : 'Unknown Movie';
-            $seatNumbers = explode(",", $booking['seat_numbers']);
-
-            foreach ($seatNumbers as $seat) {
-                if (!isset($movieSeats[$movieTitle][$seat])) {
-                    $movieSeats[$movieTitle][$seat] = array();
-                }
-                $seatType = convertSeatType($seat);
-                if (!isset($movieSeats[$movieTitle][$seat][$seatType])) {
-                    $movieSeats[$movieTitle][$seat][$seatType] = 0;
-                }
-                $movieSeats[$movieTitle][$seat][$seatType]++;
-            }
+            $movieTitle = isset($movieDetails['title']) ? $movieDetails['title'] : 'Unknown Movie'; 
+            
+            $seats = $booking['seats'];
+            $totalSeats = array_sum(array_map('intval', $seats));
 
             echo "<tr>
                 <td>{$booking['date']}</td>
